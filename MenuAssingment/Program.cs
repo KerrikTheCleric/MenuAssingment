0354e4382f0da2e1c67220b62a88be71971a56cf
Victor Gustafsson <kerrik.vg@gmail.com>
2024-05-22 16:43:26 +0200

    Comments and cleanup.

--- MenuAssingment/Program.cs
+++ MenuAssingment/Program.cs
@@ -2,6 +2,11 @@
 
 namespace MenuAssingment {
     internal class Program {
+
+        public const int YoungCustomerMovieTicketPrice = 80;
+        public const int OldCustomerMovieTicketPrice = 90;
+        public const int StandardCustomerMovieTicketPrice = 120;
+
         static void Main(string[] args) {
             bool displayMenu = true;
 
@@ -10,16 +15,19 @@ namespace MenuAssingment {
             }
         }
 
+        /// <summary>
+        /// Container for the main menu of the program that calls all other functionality.
+        /// </summary>
+        /// <returns>
+        /// False, if the program should stop after user input, true otherwise.
+        /// </returns>
+        /// <remarks>
+        /// Can be extended with new cases, but that requires the addition of new lines in PrintMainMenu.
+        /// </remarks>
+
         private static bool MainMenu() {
-            Console.WriteLine("Detta är huvudmenyn. Välj ett alternativ genom att knappa in rätt siffra nedanför:\n");
 
-            Console.WriteLine("0) Avsluta");
-            Console.WriteLine("1) Filmbiljett För En Person");
-            Console.WriteLine("2) Filmbiljetter För En Grupp");
-            Console.WriteLine("3) Repetera 10 Gånger");
-            Console.WriteLine("4) Det Tredje Ordet");
-
-
+            PrintMainMenu();
 
             int parsedResult = int.TryParse(Console.ReadLine(), out parsedResult) ? parsedResult : -1;
 
@@ -45,6 +53,23 @@ namespace MenuAssingment {
             }
         }
 
+        /// <summary>
+        /// Print method for the main menu.
+        /// </summary>
+
+        private static void PrintMainMenu() {
+            Console.WriteLine("Detta är huvudmenyn. Välj ett alternativ genom att knappa in rätt siffra nedanför:\n");
+            Console.WriteLine("0) Avsluta");
+            Console.WriteLine("1) Filmbiljett För En Person");
+            Console.WriteLine("2) Filmbiljetter För En Grupp");
+            Console.WriteLine("3) Repetera 10 Gånger");
+            Console.WriteLine("4) Det Tredje Ordet");
+        }
+
+        /// <summary>
+        /// Functionality for telling the user movie ticket prices depending on their age.
+        /// </summary>
+
         private static void MovieTicket() {
             Console.WriteLine("\nVälkommen till Rymdbio. Knappa in din ålder nedanför:");
             int parsedResultAge = int.TryParse(Console.ReadLine(), out parsedResultAge) ? parsedResultAge : -1;
@@ -52,17 +77,21 @@ namespace MenuAssingment {
             if (parsedResultAge != -1) {
 
                 if (parsedResultAge < 20) {
-                    Console.WriteLine("Ungdomspris: 80kr");
+                    Console.WriteLine("Ungdomspris: {0}kr", YoungCustomerMovieTicketPrice);
                 } else if (parsedResultAge > 64) {
-                    Console.WriteLine("Pensionärspris: 90kr");
+                    Console.WriteLine("Pensionärspris: {0}kr", OldCustomerMovieTicketPrice);
                 } else {
-                    Console.WriteLine("Standardpris: 120kr");
+                    Console.WriteLine("Standardpris: {0}kr", StandardCustomerMovieTicketPrice);
                 }
             } else {
                 Console.WriteLine("Ingen ålder inskriven, återvänder till huvudmenyn.");
             }
             Console.WriteLine("");
         }
+
+        /// <summary>
+        /// Functionality for telling the user movie ticket prices for an entire group of people of varying age.
+        /// </summary>
 
         private static void GroupMovieTickets() {
             Console.WriteLine("\nVälkommen till Rymdbio. Knappa in antalet personer i sällskapet:");
@@ -93,16 +122,32 @@ namespace MenuAssingment {
                         Console.WriteLine("Ingen ålder inmatad, återvänder till huvudmenyn.\n");
                         return;
                     }
-
                 }
 
-                Console.WriteLine("Totalkostnad för hela sällskapet på {0} personer: {1}kr", parsedResultGroupCount,
-                    (youngCustomers * 80 + oldCustomers * 90 + standardCustomers * 120));
+                Console.WriteLine("Totalkostnad för hela sällskapet på {0} personer: {1}kr", parsedResultGroupCount, CalculateGroupPrice(youngCustomers, oldCustomers, standardCustomers));
             } else {
                 Console.WriteLine("Inget antal över 0 inskrivet, återvänder till huvudmenyn.");
             }
             Console.WriteLine("");
         }
+
+        /// <summary>
+        /// Helper method for GroupMovieTickets which calculates the group ticket price.
+        /// </summary>
+        /// <param name="youngCustomers">Amount of young customers.</param>
+        /// <param name="oldCustomers">Amount of old customers.</param>
+        /// <param name="standardCustomers">Amount of standard customers.</param>
+        /// <returns>
+        /// The total ticket price for the group.
+        /// </returns>
+
+        private static int CalculateGroupPrice(int youngCustomers, int oldCustomers, int standardCustomers) {
+            return youngCustomers * YoungCustomerMovieTicketPrice + oldCustomers * OldCustomerMovieTicketPrice + standardCustomers * StandardCustomerMovieTicketPrice;
+        }
+
+        /// <summary>
+        /// Repeatedly prints out the given user input ten times on the same line.
+        /// </summary>
 
         private static void RepeatTenTimes() {
             Console.WriteLine("\nSkriv in vad du vill repetera: ");
@@ -120,6 +165,10 @@ namespace MenuAssingment {
             }
             Console.WriteLine("");
         }
+
+        /// <summary>
+        /// Asks the user for a sentence containing at least 3 words and then prints out the third word.
+        /// </summary>
 
         private static void TheThirdWord() {
             Console.WriteLine("\nSkriv in en mening med minst tre ord: ");
